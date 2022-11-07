55bc7ec1f17f91a65a674b334a87f94932a85ded
Wink Saville <wink@saville.com>
2022-11-07 14:00:50 -0800

    feat: Add circular reference
    Not used ATM and this could be WRONG :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-struct S1 {
+use std::{rc::Rc, cell::RefCell};
+
+#[allow(unused)]
+struct S1<'a> {
+    s2: Option<&'a Rc<RefCell<S2<'a>>>>,
     v1: i32
 }
 
-impl S1 {
-    fn new() -> Self {
+impl<'a> S1<'a> {
+    fn new(s2: Option<&'a Rc<RefCell<S2<'a>>>>) -> Self {
         Self {
+            s2,
             v1: 0,
         }
     }
@@ -14,13 +19,16 @@ impl S1 {
     }
 }
 
-struct S2 {
+#[allow(unused)]
+struct S2<'a> {
+    s1: Option<&'a Rc<RefCell<S1<'a>>>>,
     v2: i32
 }
 
-impl S2 {
-    fn new() -> Self {
+impl<'a> S2<'a> {
+    fn new(s1: Option<&'a Rc<RefCell<S1<'a>>>>) -> Self {
         Self {
+            s1,
             v2: 0,
         }
     }
@@ -30,10 +38,11 @@ impl S2 {
     }
 }
 
+
 fn main() {
-    let mut s1 = S1::new();
+    let mut s1 = S1::new(None);
     s1.add(-1);
-    let mut s2 = S2::new();
+    let mut s2 = S2::new(None);
     s2.add(1);
 
     println!("s1.v1={} s2.v2={}", s1.v1, s2.v2);
@@ -45,13 +54,13 @@ mod test {
 
     #[test]
     fn test() {
-        let mut s1 = S1::new();
+        let mut s1 = S1::new(None);
         assert_eq!(s1.v1, 0);
         s1.add(1);
         assert_eq!(s1.v1, 1);
 
 
-        let mut s2 = S2::new();
+        let mut s2 = S2::new(None);
         assert_eq!(s2.v2, 0);
         s2.add(1);
         assert_eq!(s2.v2, 1);
